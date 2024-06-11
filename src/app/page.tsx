a699cc285e8702521478a2f69c0e810d5d883bf9
8harrison <monteiro4100@gmail.com>
2024-06-11 13:18:56 -0300

    icone e mudança de regra de layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,19 @@ import Header from '@/components/Header';
 import Services from '@/components/Services';
 import { NavMobileProvider } from '@/components/contexts/NavMobileContext';
 
+
 export default function Home() {
   return (
     <NavMobileProvider>
-
-    <div style={{ backgroundColor: ' rgb(224, 214, 200)' }}>
-      <Header />
-      <Banner/>
-      <Description/>
-      <Services/>
-      <About/>
-      <Contact/>
-      <Footer />
-    </div>
-    </NavMobileProvider> 
+      <div style={{ backgroundColor: ' rgb(224, 214, 200)' }}>
+        <Header />
+        <Banner />
+        <Description />
+        <Services />
+        <About />
+        <Contact />
+        <Footer />
+      </div>
+    </NavMobileProvider>
   );
 }
